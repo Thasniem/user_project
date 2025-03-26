020b25de70e6c90c4ea63cbd4118c132622d904f
Thasniem <thfjd7865@gmail.com>
2025-03-26 14:21:14 +0530

    updated

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,12 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+app_name = 'user_app'
+
+# Create a router and register our viewsets with it
 router = DefaultRouter()
 router.register(r'auth', views.AuthViewSet, basename='auth')
 router.register(r'users', views.UserViewSet, basename='user')
 router.register(r'profiles', views.ProfileViewSet, basename='profile')
-router.register(r'activities', views.ActivityLogViewSet, basename='activity')
+router.register(r'activity-logs', views.ActivityLogViewSet, basename='activity-log')
 
 urlpatterns = [
+    # Include the router URLs
     path('', include(router.urls)),
 ]
